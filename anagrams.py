07b73bd7aa28a068180347b2c2b04dece4b5afe3
Will Lockhart <31291365+WSLockhart@users.noreply.github.com>
2017-08-24 20:43:01 -0700

    Add files via upload

--- anagrams.py
+++ anagrams.py
@@ -1,17 +1,17 @@
-# ANAGRAMS PROGRAM
+# ANAGRAMS PROGRAM v1.0
 # Finds all words in the dictionary that are valid steals of the input word
 # Algorithm uses a map from letters to primes
 # to convert stealability into divisibility!
 # --------------------------------------------------------------
 
-# access the command line input
+# Access the command line input
 import sys
 
 
-# return a list of the first n prime numbers.
-# note: this alrorithm could be much for efficient (sieve)
-# but we don't need very many primes so it doesn't much matter
+# Return a list of the first n prime numbers
 def list_primes(n):
+	# note: this alrorithm could be much for efficient (sieve)
+	# but we don't need very many primes so it doesn't much matter
 	primelist = []
 	count = 0
 	k = 2	# k tracks which number we're checking for primeness
@@ -31,9 +31,9 @@ def list_primes(n):
 	return primelist
 
 
-# returns an integer. This number will encode the input character string.
-# It's the product of the primes associated with each letter:
-# 'a' -> 2, 'b' -> 3, 'c' -> 5, 'd' -> 7...
+# Takes a character string and a list of numbers (primes), and returns an integer.
+# The string is encoded as the product of the prime numbers associated with each letter:
+# 'a' -> 2, 'b' -> 3, 'c' -> 5,..., 'z' -> 103.
 def encode(chstring, theprimes):
 	codenumber = 1
 	for ch in chstring:
@@ -41,19 +41,19 @@ def encode(chstring, theprimes):
 		# if 0 <= k and k <= 25:   #make sure it's a regular lower-case letter
 		p = theprimes[k]
 		# else: p = 107	# z-->103. So the next prime, 107, will be our
-						# catch-all code for an unrecognized character.
+		# catch-all code for an unrecognized character.
 		codenumber *= p
 	return codenumber
 
 
-# return a list of all valid steals on the input word ("keyword")
+# Return a list of all valid steals of the input word ("keyword")
 def findsteals(keyword):
 	steal_list = []  # list starts empty
 
 	theprimes = list_primes(200)
-	# normally only need 26, but there might be weird characters in the dictionary
+	# should only need 26, but there might be weird characters in the dictionary
 	# like accents or capitals or whatever, so this way we get them all
-	# and we don't have to verify that every character is valid - commented out in encode()
+	# and we don't have to bother checking that every character is valid ( commented out in encode() )
 
 	codenumber = encode(keyword, theprimes)  #encode the word as a number
 
@@ -75,19 +75,19 @@ def findsteals(keyword):
 	return steal_list
 
 
-# take a file name, open it, and return a list of the words in that file
+# Take a file name, open it, and return a list of the words in that file
 def load_dictionary(filename):
 	txtfile = open(filename, 'r')  # 'r' is the "read" mode, as opposed to "write" or something else
 	# Exactly how this is handled depends on the formatting of the dictionary.
 	# For the engmix_84k word list, python's read() function interprets it as
-	# a single character string with spaces between each word (rather than linebreaks).
+	# a single character string, with spaces (rather than linebreaks) between each word.
 	# The split() function creates a new list item every time it hits a space.
 	word_list = txtfile.read().split()
 	txtfile.close()
 	return word_list
 
 
-# check if input is a valid character string
+# Check if input is a valid character string
 def is_string(s):
 	try:
 		str(s)
@@ -96,9 +96,9 @@ def is_string(s):
 		return False
 
 
-# main function, called at launch
+# Main function, called upon launch
 def main():
-	#check that there is only one item entered
+	# check that only a single word has been entered
 	if len(sys.argv) == 2:
 		input = sys.argv[1]
 		# check that it's a valid character string
@@ -108,7 +108,7 @@ def main():
 			final_list = findsteals(inputword)
 			print str(len(final_list)) + ' possible steals found for ' + inputword + ': ' + str(final_list)
 		else:
-			print 'entry invalid'
+			print 'invalid entry!'
 
 
 # This is the standard boilerplate that calls the main() function.
